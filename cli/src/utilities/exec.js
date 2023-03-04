8cbefb62d55b8e269c6cffbf88fc3bd63957616e
Eleanor <32818082+0@users.noreply.github.com>
2023-03-04 15:37:03 +0000

    chore(cli/utilities): update exec

--- cli/src/utilities/exec.js
+++ cli/src/utilities/exec.js
@@ -1,13 +1,13 @@
 const shell = require('shelljs')
 
-module.exports = (command) => {
+module.exports = (command, cwd) => {
   try {
-    const out = shell.exec(command, { silent: true })
+    const out = shell.exec(command, { silent: true, cwd })
 
     if (out.stderr) {
       throw new Error(out.stderr.trim())
     }
-    return undefined
+    return out.stdout.trim()
   } catch (err) {
     return err
   }
