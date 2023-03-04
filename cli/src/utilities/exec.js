9482f1442dfe6ab3c835b2eaa1587ff136591f74
Eleanor <32818082+0@users.noreply.github.com>
2023-03-04 16:11:15 +0000

    chore(cli/utilities): make `silent` an option, defaults true

--- cli/src/utilities/exec.js
+++ cli/src/utilities/exec.js
@@ -1,9 +1,9 @@
 const process = require('node:process')
 const shell = require('shelljs')
 
-module.exports = (command, cwd = process.cwd()) => {
+module.exports = (command, silent = true, cwd = process.cwd()) => {
   return new Promise((resolve, reject) => {
-    const out = shell.exec(command, { silent: true, cwd })
+    const out = shell.exec(command, { silent, cwd })
 
     if (out.stderr) reject(out.stderr.trim())
     else resolve(out.stdout.trim())
