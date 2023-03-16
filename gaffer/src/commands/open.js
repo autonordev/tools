30c08915a89af51c829a1c5f9d0938f8e843a60b
Eleanor <32818082+0@users.noreply.github.com>
2023-03-16 09:18:53 +0000

    feat(gaffer): do not allow model files to be opened

--- gaffer/src/commands/open.js
+++ gaffer/src/commands/open.js
@@ -1,3 +1,4 @@
+const path = require('node:path')
 const log = require('../helpers/log')
 const build = require('../functions/build')
 const open = require('../functions/open')
@@ -20,6 +21,13 @@ module.exports = (program) => {
 
         const project = state.schemes.get(projectName)
         const buildPath = project.outputs.build
+
+        const ext = path.extname(buildPath)
+        if (ext === 'rbxm' || ext === 'rbxmx') {
+          throw new Error(
+            `[Gxxx] Project \`${projectName}\` could not be opened, as it outputs a model.`
+          )
+        }
 
         await open(buildPath)
         log.success(`Built and opened file \`${buildPath}\``)
