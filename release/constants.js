7f27eec9941a80a053e57be181285edceb3cf514
Eleanor <32818082+0@users.noreply.github.com>
2023-03-19 16:49:27 +0000

    fix(ci): use node18

--- release/constants.js
+++ release/constants.js
@@ -3,7 +3,7 @@ const path = require('node:path')
 module.exports = {
   TOOLS: { gaffer: { path: path.resolve(__dirname, '../gaffer') } },
 
-  NODE_RANGE: 'node16',
+  NODE_RANGE: 'node18',
 
   // What foreman/aftman wants on the left, and what pkg wants on the right.
   // -x86_64 may be added after the os name but this seems optional.
